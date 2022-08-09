cf856d89af345867afee1151469e97d136e2e9d6
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2022-08-09 17:34:12 +0800

    Integrate ProvenDB

--- src/apolloApplication.js
+++ src/apolloApplication.js
@@ -7,7 +7,6 @@ import { HTTPResponseModule } from "../types/HTTPResponse.js"
 import { PolicyModule } from "../types/Policy.js"
 import { PolicyTakenModule } from "../types/PolicyTaken.js"
 import { UserModule } from "../types/User.js"
-
 export const apolloApplication = createApplication({
 	modules: [
 		AgentModule,
